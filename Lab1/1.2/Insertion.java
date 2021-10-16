75d894791323dd59336f0aa804725c18639ae033
TheScorpoi <sobral@ua.pt>
2021-10-16 23:22:36 +0100

    ex 1.3 done

---
+++ Lab1/1.2/Insertion.java
@@ -0,0 +1,37 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Insertion {
+
+    public static void main(String[] args) {
+        Map<Character, Integer> nameMap = new HashMap<>();
+
+        try (Scanner sc = new Scanner(new File("./Lab1/1_2/names.txt"))) {
+            try (PrintWriter out = new PrintWriter(new File("./Lab1/1_2/names_counting.txt"))) {
+                while (sc.hasNextLine()) {
+                    String name = sc.nextLine();
+                    if (!nameMap.containsKey(name.charAt(0))) {
+                        nameMap.put(name.charAt(0), 1);
+                    } else {
+                        nameMap.put(name.charAt(0), nameMap.get(name.charAt(0)) + 1);
+                    }
+                }
+                
+                for (Character key : nameMap.keySet()) {
+                    out.println("SET " + Character.toUpperCase(key) + " " + nameMap.get(key));
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
+            System.exit(0);
+        }
+
+    }
+
+}
--- Lab1/1.3/src/main/java/com/cbd/SimplePost.java
+++ Lab1/1.3/src/main/java/com/cbd/SimplePost.java
@@ -41,7 +41,6 @@ public class SimplePost {
         boardMap.getAllKeys().stream().forEach(System.out::println);
 
     }
-
 }
 
 class SimplePostSet {
