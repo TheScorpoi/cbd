402715ed522648809b2e5fc34cbdfd883d9adaab
TheScorpoi <sobral@ua.pt>
2021-11-26 15:09:21 +0000

    some files

---
+++ Lab3/use Teste1.sql
@@ -0,0 +1,10 @@
+create table users {
+    usarname varchar,
+    firstname varchar,
+    lastname varchar,
+    Primary key (usarname)
+}
+
+insert into users values ("Thescorpoi", "Pedro", "Sobral");
+
+Select * from users;
